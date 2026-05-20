90e9dc3db5bd465740bf0393f32bc72990f45ebe
Xuân Trường <alonesera1612@gmail.com>
2026-05-20 10:45:28 +0000

    bai tap buoi 2

---
+++ node-app/src/index.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        message: "Chào mừng bạn đến với Node.js + TypeScript Server!"
+    });
+});
+
+app.post("/api/post", (req: Request, res: Response) => {
+    const dataFromClient = req.body;
+
+    console.log("Dữ liệu nhận được từ Client:", dataFromClient);
+
+    res.status(200).json({
+        success: true,
+        message: "Server đã nhận được POST Request thành công!",
+        receivedData: dataFromClient
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
