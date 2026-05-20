002cb3cd6e39f4a5c0ff1c5b791dcc76e79a713c
Xuân Trường <alonesera1612@gmail.com>
2026-05-20 11:05:45 +0000

    bai tap buoi 2

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/api/get", (req: Request, res: Response) => {
     res.status(200).json({
         message: "Chào mừng bạn đến với Node.js + TypeScript Server!"
     });
