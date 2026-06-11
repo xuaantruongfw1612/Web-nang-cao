3bd1813145504848679e3959040a6ab977145a87
Đặng Tài <dangductai0510@gmail.com>
2026-06-11 13:42:14 +0000

    add notification-log.entity.ts

---
+++ last-dance/src/notification-log.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn, CreateDateColumn } from 'typeorm';
+import { Task } from './task.entity';
+
+@Entity()
+export class NotificationLog {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 20 })
+  milestone: string; 
+  @CreateDateColumn()
+  sentAt: Date; 
+
+  @ManyToOne(() => Task, task => task.notificationLogs, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'task_id' })
+  task: Task;
+}
